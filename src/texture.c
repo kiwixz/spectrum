3e164a24ea86e8060e3f814ee90525970c2aca0d
kiwixz <kiwixz@users.noreply.github.com>
2015-03-29 15:34:29 +0200

    font texture fix

--- src/texture.c
+++ src/texture.c
@@ -22,76 +22,7 @@
 #include "texture.h"
 #include "shared.h"
 
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-  #define UCTOUI(c) \
-  ((c)[3] << 24)    \
-  | ((c)[2] << 16)  \
-  | ((c)[1] << 8)   \
-  | ((c)[0])
-#elif __BYTE_ORDER == __BIG_ENDIAN
-  #define CTOUI(c) \
-  ((c)[0] << 24)   \
-  | ((c)[1] << 16) \
-  | ((c)[2] << 8)  \
-  | ((c)[3])
-#else
-  #error Could not determine endianness !
-#endif
-
 static GLuint texs[TEXTURES_LENGTH];
-
-#if 0
-  static int read_send_tex(const char *file)
-  {
-    unsigned int  w, h, size, mipmaps, len, offset, level;
-    unsigned char header[128];
-    GLchar        *buffer;
-    FILE          *f;
-
-    f = fopen(file, "rb");
-    if (!f)
-      {
-        ERROR("Failed to open a texture.");
-        return -1;
-      }
-
-    fread(header, sizeof(unsigned char), 128, f);
-
-    h = UCTOUI(header + 12);
-    w = UCTOUI(header + 16);
-    size = UCTOUI(header + 20);
-    mipmaps = UCTOUI(header + 28);
-
-    len = size * 2;
-    buffer = malloc(len * sizeof(GLchar));
-    if (!buffer)
-      {
-        ERROR("Failed to malloc buffer.");
-        return -1;
-      }
-
-    fread(buffer, sizeof(GLchar), len, f);
-    fclose(f);
-
-    offset = 0;
-    for (level = 0; level < mipmaps && (w || h);
-         ++level)
-      {
-        glCompressedTexImage2D(GL_TEXTURE_2D, level,
-                               GL_COMPRESSED_RGBA_S3TC_DXT5_EXT,
-                               w, h, 0, size, buffer + offset);
-
-        offset += size;
-        w /= 2;
-        h /= 2;
-        size = ((w + 3) / 4) * ((h + 3) / 4) * 16;
-      }
-
-    free(buffer);
-    return 0;
-  }
-
-#endif
 
 static int read_send_tex(const char *file) // read only ppm
 {
@@ -149,7 +80,6 @@ static int read_send_tex(const char *file) // read only ppm
     }
 
   GOAFTER('\n');
-  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, w, h, 0, GL_RGB,
                GL_UNSIGNED_BYTE, buffer + offset);
 
@@ -166,13 +96,15 @@ static int create_texture(int index, const char *file)
   glBindTexture(GL_TEXTURE_2D, tex);
 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
-                  GL_NEAREST_MIPMAP_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                  GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   if (read_send_tex(file))
     return -1;
 
+  glGenerateMipmap(GL_TEXTURE_2D);
   glBindTexture(GL_TEXTURE_2D, 0);
   texs[index] = tex;
+
   return 0;
 }
 
