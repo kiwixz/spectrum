a397d227752c25416042f3adacc1a298991b9e0b
kiwixz <kiwixz@users.noreply.github.com>
2015-04-04 19:03:16 +0200

    INVALID_ENUM OpenGL error fix

--- src/texture.c
+++ src/texture.c
@@ -95,9 +95,10 @@ static int create_texture(int index, const char *file)
   glGenTextures(1, &tex);
   glBindTexture(GL_TEXTURE_2D, tex);
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                   GL_LINEAR_MIPMAP_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
   if (read_send_tex(file))
     return -1;
 
